1d49362c90c1be3da465f049a8eabcc96d066653
NingAnMe <ninganme@qq.com>
2019-08-12 18:01:54 +0800

    增加获取数据和获取最近点的功能

--- lib/lib_get_index_by_lonlat.py
+++ lib/lib_get_index_by_lonlat.py
@@ -4,3 +4,67 @@
 @Time    : 2019/8/12
 @Author  : AnNing
 """
+
+import pickle
+
+import numpy as np
+from pykdtree import kdtree
+
+
+
+
+def make_point_index_lut(lons_data, lats_data):
+    condition = np.logical_and(np.isfinite(lons_data), np.isfinite(lats_data))
+    idx = np.where(condition)
+    lon_new = lons_data[idx]
+    lat_new = lats_data[idx]
+    lons_lats = zip(lon_new.reshape(-1, ), lat_new.reshape(-1, ))
+    data = lons_lats
+    print('start cKDTree')
+    ck = kdtree.KDtree(data)
+    with open('index_lut.pickle') as fp:
+        pickle.dump((idx, ck), fp)
+
+
+def get_point_index(lon, lat, index_lut_file, pre_dist=0.04):
+    with open(index_lut_file) as fp:
+        idx, ck = pickle.load(fp)
+    fix_point = (lon, lat)
+    dist, index = ck.query([fix_point], 1)
+    dist = dist[0]
+    index = index[0]
+    print('---Query INFO---dist: {}  index: {}'.format(dist, index))
+
+    if dist <= pre_dist:
+        fix_point_index = (idx[0][index], idx[1][index])
+        print("---INFO---Nearest fix point index={}".format(fix_point_index))
+        return fix_point_index
+    else:
+        print('***WARNING*** dist > {}, Dont extract.'.format(pre_dist))
+
+
+def get_area_index(lons=None, lats=None, left_up_lon=None, left_up_lat=None, right_down_lon=None, right_down_lat=None):
+    lon_min = left_up_lon
+    lon_max = right_down_lon
+    lat_min = right_down_lat
+    lat_max = left_up_lat
+    index = np.where(np.logical_and.reduce((lons > lon_min, lons < lon_max, lats > lat_min, lats < lat_max)))
+    row_min = np.min(index[0])
+    row_max = np.max(index[0])
+    col_min = np.min(index[1])
+    col_max = np.max(index[1])
+    return (row_min, row_max), (col_min, col_max)
+
+
+def get_data_by_index(data=None, row_min=237, row_max=1373, col_min=497, col_max=2262):
+    """
+    默认为4
+    :param data:
+    :param row_min:
+    :param row_max:
+    :param col_min:
+    :param col_max:
+    :return:
+    """
+    if data is not None:
+        return data[row_min:row_max+1, col_min:col_max+1]
